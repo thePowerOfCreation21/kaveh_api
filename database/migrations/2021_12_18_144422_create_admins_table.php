fa8965e09bd9fc070d12fe2ac9c6af06e6c8b570
thePowerOfCreation21 <dadekavweb2@gmail.com>
2021-12-18 18:25:40 +0330

    first commit (i created admins table migration too)

---
+++ database/migrations/2021_12_18_144422_create_admins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_name', 25)->unique();
+            $table->string('password', 150);
+            $table->boolean('is_primary')->default(false);
+            $table->json('privilege');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
