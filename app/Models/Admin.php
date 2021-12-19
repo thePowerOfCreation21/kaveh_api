0664a95cb614cae53379da49a1fda710ff05fd6d
thePowerOfCreation21 <dadekavweb2@gmail.com>
2021-12-19 07:32:18 +0330

    created admin model & controller

---
+++ app/Models/Admin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Admin extends Model
+{
+    use HasFactory;
+
+    protected $table = 'admins';
+
+    protected $fillable = [
+        'user_name',
+        'password',
+        'privilege'
+    ];
+
+    protected $casts = [
+        'privilege' => 'array'
+    ];
+
+    public $timestamps = false;
+}
