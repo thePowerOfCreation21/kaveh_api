17ce13a77b2bd1a360e7b98502ac76fd350398fc
thePowerOfCreation21 <dadekavweb2@gmail.com>
2021-12-20 19:41:52 +0330

    created AdminChangesHistory model

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Admin;
-use Illuminate\Support\Facades\Hash;
 use App\Services\AdminService;
 
 class AdminController extends Controller
---
+++ app/Models/AdminChangesHistory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Admin;
+
+class AdminChangesHistory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'admin_changes_history';
+
+    protected $fillable = [
+        'doer_id',
+        'subject_id',
+        'action',
+        'date'
+    ];
+
+    public $timestamps = false;
+
+    public function doer ()
+    {
+        return $this->hasOne(Admin::class, 'id', 'doer_id');
+    }
+
+    public function subject ()
+    {
+        return $this->hasOne(Admin::class, 'id', 'subject_id');
+    }
+}
