5e27fb0ab77750c5caa7f3d08df54929882d0351
thePowerOfCreation21 <dadekavweb2@gmail.com>
2021-12-20 17:36:19 +0330

    created admin changes history table (i will save every changes each admin does on each account)

---
+++ database/migrations/2021_12_20_135025_create_admin_changes_history_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminChangesHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_changes_history', function (Blueprint $table) {
+            $table->id();
+            $table->string('doer_id', 25);
+            $table->string('subject_id', 25);
+            $table->string('action', 50);
+            $table->string('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_changes_history');
+    }
+}
