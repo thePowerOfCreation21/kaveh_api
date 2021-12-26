89e5a57bcaf18cabc32518a206c9871c1720289d
thePowerOfCreation21 <dadekavweb2@gmail.com>
2021-12-26 07:31:48 +0330

    created GalleryImage model

---
+++ app/Models/GalleryImage.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GalleryImage extends Model
+{
+    use HasFactory;
+
+    protected $table = 'gallery_images';
+
+    protected $fillable = [
+        'image'
+    ];
+}
