9986dbf87299651153c84637109894d4405b264e
thePowerOfCreation21 <dadekavweb2@gmail.com>
2021-12-27 18:32:01 +0330

    created Article model

---
+++ app/Models/Articles.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Casts\CustomDateCast;
+
+class Articles extends Model
+{
+    use HasFactory;
+
+    protected $table = 'articles';
+
+    protected $fillable = [
+        'title',
+        'image',
+        'content'
+    ];
+
+    protected $casts = [
+        'created_at' => CustomDateCast::class,
+        'updated_at' => CustomDateCast::class
+    ];
+}
