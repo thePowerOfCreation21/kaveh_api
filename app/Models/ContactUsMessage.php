719c700d708f946321694340706f7f927fe365c9
thePowerOfCreation21 <dadekavweb2@gmail.com>
2021-12-27 18:03:59 +0330

    custom casting created_at value of ContactUsMessage

--- app/Models/ContactUsMessage.php
+++ app/Models/ContactUsMessage.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\CustomDateCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,4 +21,18 @@ class ContactUsMessage extends Model
     protected $hidden = [
         'updated_at'
     ];
+
+    protected $casts = [
+        'created_at' => CustomDateCast::class
+    ];
+
+    public $timestamps = false;
+
+    public function __construct(array $attributes = [])
+    {
+        $this->setRawAttributes([
+            'created_at' => date("Y-m-d H:i:s")
+        ], true);
+        parent::__construct($attributes);
+    }
 }
