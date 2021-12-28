5775500784f8974bd208768e6cbfc51303dfc40a
thePowerOfCreation21 <dadekavweb2@gmail.com>
2021-12-28 16:31:36 +0330

    created branches table

---
+++ database/migrations/2021_12_28_125720_create_branches_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBranchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->string('description');
+            $table->string('address');
+            $table->string('image', 1500);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branches');
+    }
+}
