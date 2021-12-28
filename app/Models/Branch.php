40a3ad69f18a8462fec57658e389791ecff35720
thePowerOfCreation21 <dadekavweb2@gmail.com>
2021-12-28 16:39:53 +0330

    created Branch model

---
+++ app/Models/Branch.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Branch extends Model
+{
+    use HasFactory;
+
+    protected $table = 'branches';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'address',
+        'image',
+    ];
+
+    public $timestamps = false;
+}
