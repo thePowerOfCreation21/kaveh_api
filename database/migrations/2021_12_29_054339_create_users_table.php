1d8b5b7330f2a8f8e6db9dfca204d0d650cb0aff
thePowerOfCreation21 <dadekavweb2@gmail.com>
2021-12-29 09:29:12 +0330

    created users table

---
+++ database/migrations/2021_12_29_054339_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64);
+            $table->string('last_name', 64);
+            $table->string('phone_number')->unique();
+            $table->string('second_phone_number')->nullable();
+            $table->string('password');
+            $table->string('area')->nullable();
+            $table->string('card_number', 16)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
