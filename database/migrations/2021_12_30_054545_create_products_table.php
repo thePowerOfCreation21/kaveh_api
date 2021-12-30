9c62e9ef61b5e4a21a9bdd4e88baea8e61c46f86
thePowerOfCreation21 <dadekavweb2@gmail.com>
2021-12-30 09:31:57 +0330

    created products table

---
+++ database/migrations/2021_12_30_054545_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 128);
+            $table->string('image', 1500);
+            $table->string('description', 500)->nullable();
+            $table->integer('price')->unsigned();
+            $table->integer('discount_percentage')->default(0);
+            $table->string('type')->default('unlimited');
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
