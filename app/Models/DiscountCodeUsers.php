cd4d57d9fd43081d2ada85ea035735f30449a65c
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-01-15 13:42:53 +0330

    added some details to DiscountCodeUsers model

--- app/Models/DiscountCodeUsers.php
+++ app/Models/DiscountCodeUsers.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\DiscountCode;
 
 class DiscountCodeUsers extends Model
 {
     use HasFactory;
+
+    protected $table = 'discount_code_users';
+
+    protected $fillable = [
+        'user_id',
+        'discount_code',
+        'is_used'
+    ];
+
+    protected $casts = [
+        'is_used' => 'boolean'
+    ];
+
+    public function user ()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    public function discount_code ()
+    {
+        return $this->hasOne(DiscountCode::class, 'code', 'discount_code');
+    }
 }
