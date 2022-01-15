c3e97937295a37efe873f26f53ff6388d7775b5e
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-01-15 15:08:47 +0330

    changed logic of middleware reponsible for checking if user has all required privileges, so now it accepts or with | symbol

--- app/Http/Middleware/RequiredPrivilege.php
+++ app/Http/Middleware/RequiredPrivilege.php
@@ -14,26 +14,36 @@ class RequiredPrivilege
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next, string $needed_privilege)
+    public function handle(Request $request, Closure $next, string $privileges_string)
     {
-        $needed_privileges = explode('&', $needed_privilege);
         $user = $request->user();
-        if ($user->is_primary)
+
+        if (! $user->is_primary)
         {
-            return $next($request);
-        }
-        foreach ($needed_privileges as $needed_privilege)
-        {
-            if (! in_array($needed_privilege, $user->privileges))
+            $or_privileges = explode('|', $privileges_string);
+            $last_index_of_or_privileges = count($or_privileges) - 1;
+
+            foreach ($or_privileges AS $or_privilege_key => $or_privilege)
             {
-                return response([
-                    'code' => 4,
-                    'message' => 'you do not have permission to access this part of api',
-                    'needed_privileges' => $needed_privileges,
-                    'your_privileges' => $user->privileges
-                ], 403);
+                $privileges = explode('&', $or_privileges);
+                foreach ($privileges AS $privilege_key => $privilege)
+                {
+                    if (! in_array($privilege, $user->privileges))
+                    {
+                        if ($or_privilege_key == $last_index_of_or_privileges)
+                        {
+                            return response([
+                                'code' => 4,
+                                'message' => 'you do not have permission to access this part of api',
+                                'required_privilege' => $privilege
+                            ], 403);
+                        }
+                        break;
+                    }
+                }
             }
         }
+
         return $next($request);
     }
 }
