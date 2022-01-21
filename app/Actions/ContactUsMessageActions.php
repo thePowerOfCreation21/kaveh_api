347cfd63a06c6a89d329705303cc893d2ddea0b6
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-01-21 09:44:00 +0330

    will store contact us messages with ContactUsMessageActions

--- app/Actions/ContactUsMessageActions.php
+++ app/Actions/ContactUsMessageActions.php
@@ -3,9 +3,21 @@
 namespace App\Actions;
 
 use App\Models\ContactUsMessage;
+use Illuminate\Http\Request;
 
 class ContactUsMessageActions
 {
+    public static function store_with_request (Request $request)
+    {
+        return ContactUsMessage::create(
+            $request->validate([
+                'full_name' => 'required|string|max:120',
+                'email' => 'required|email|max:120',
+                'message' => 'required|string|max:120'
+            ])
+        );
+    }
+
     public static function get_all (int $skip = 0, int $limit = 50)
     {
         return (object) [
--- app/Http/Controllers/ContactUsMessageController.php
+++ app/Http/Controllers/ContactUsMessageController.php
@@ -10,13 +10,7 @@ class ContactUsMessageController extends Controller
 {
     public function store (Request $request)
     {
-        ContactUsMessage::create(
-            $request->validate([
-                'full_name' => 'required|string|max:120',
-                'email' => 'required|email|max:120',
-                'message' => 'required|string|max:120'
-            ])
-        );
+        ContactUsMessageActions::store_with_request($request);
 
         return response()->json([
             'message' => 'message sent successfully'
