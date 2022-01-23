59b4cc5e4a658378da598343b3c7b95ca99c0b3e
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-01-23 15:02:53 +0330

    can add discount for all users

--- app/Actions/DiscountCodeActions.php
+++ app/Actions/DiscountCodeActions.php
@@ -2,14 +2,54 @@
 
 namespace App\Actions;
 
+use App\Exceptions\CustomException;
+use App\Models\DiscountCode;
 use Illuminate\Http\Request;
 
 class DiscountCodeActions
 {
-    public static function store_by_request (Request $request)
+    /**
+     * make new discount with request
+     *
+     * @param Request $request
+     * @return DiscountCode
+     * @throws CustomException
+     */
+    public static function store_by_request (Request $request): DiscountCode
     {
-        $request->validate([
-            'code' => 'required|string|unique:App\Models\Dis'
+        $discount_data = $request->validate([
+            'code' => 'required|string|max:25',
+            'amount' => 'required|numeric|min:1',
+            'type' => 'required|in:percent,price',
+            'users' => 'array|max:1000',
+            'users.*' => 'string|max:25',
+            'expiration_date' => 'date_format:Y-m-d H:i:s'
         ]);
+
+        return self::store($discount_data);
+    }
+
+    /**
+     * make new discount
+     *
+     * @param array $discount_data
+     * @return DiscountCode
+     * @throws CustomException
+     */
+    public static function store (array $discount_data): DiscountCode
+    {
+        if ($discount_data['type'] == 'percent' && $discount_data['amount'] > 100)
+        {
+            throw new CustomException('amount of percent discount could not be more than 100', 54, 400);
+        }
+
+        if (DiscountCode::where('code', $discount_data['code'])->exists())
+        {
+            throw new CustomException('this code is already taken', 52, 400);
+        }
+
+        $discount_data['is_for_all_users'] = true;
+
+        return DiscountCode::create($discount_data);
     }
 }
