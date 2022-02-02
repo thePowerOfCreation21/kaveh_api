0af292eb6e6bc9650b9e4d0578454d553a853526
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-02-02 11:50:49 +0330

    ADD notifications table migration

---
+++ database/migrations/2022_02_02_081517_create_notifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('is_for_all_users')->default(false);
+            $table->string('type', 10);
+            $table->string('text', 1500);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
