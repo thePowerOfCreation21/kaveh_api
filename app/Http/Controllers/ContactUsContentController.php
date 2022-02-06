2a35b62fc6210b80f0b391d06d06493ea39a4420
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-02-06 23:29:32 +0330

    DELETE useless used class

--- app/Http/Controllers/ContactUsContentController.php
+++ app/Http/Controllers/ContactUsContentController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ContactUsContent;
-use App\Actions\ContactUsContentActions;
 
 class ContactUsContentController extends Controller
 {
