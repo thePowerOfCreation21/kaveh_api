c7e1dbf1d67fdffe3ea0030bd5e278eb88dd216a
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-02-07 11:28:29 +0330

    ADD store_by_request method to NotificationFrameAction

--- app/Actions/NotificationFrameAction.php
+++ app/Actions/NotificationFrameAction.php
@@ -3,7 +3,9 @@
 namespace App\Actions;
 
 use App\Abstracts\Action;
+use App\Exceptions\CustomException;
 use App\Models\NotificationFrame;
+use Illuminate\Http\Request;
 
 class NotificationFrameAction extends Action
 {
@@ -18,5 +20,16 @@ class NotificationFrameAction extends Action
 
     public function __construct(){
         $this->model = NotificationFrame::class;
+    }
+
+    /**
+     * @param Request $request
+     * @param string $validation_role
+     * @return mixed
+     * @throws CustomException
+     */
+    public function store_by_request(Request $request, $validation_role = 'store')
+    {
+        return parent::store_by_request($request, $validation_role);
     }
 }
