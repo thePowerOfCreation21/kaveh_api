8b2209b002df5097f2357c7c1f1072de58b36b23
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-02-07 11:30:43 +0330

    ADD get_by_request method to NotificationFrameAction

--- app/Actions/NotificationFrameAction.php
+++ app/Actions/NotificationFrameAction.php
@@ -5,6 +5,8 @@ namespace App\Actions;
 use App\Abstracts\Action;
 use App\Exceptions\CustomException;
 use App\Models\NotificationFrame;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
 
 class NotificationFrameAction extends Action
@@ -31,5 +33,23 @@ class NotificationFrameAction extends Action
     public function store_by_request(Request $request, $validation_role = 'store')
     {
         return parent::store_by_request($request, $validation_role);
+    }
+
+    /**
+     * @param Request $request
+     * @param string|array $query_validation_role
+     * @param null|Model|Builder $eloquent
+     * @param array $order_by
+     * @return object
+     * @throws CustomException
+     */
+    public function get_by_request(
+        Request $request,
+        $query_validation_role = 'get_query',
+        $eloquent = null,
+        array $order_by = ['id' => 'DESC']
+    ): object
+    {
+        return parent::get_by_request($request, $query_validation_role, $eloquent, $order_by);
     }
 }
