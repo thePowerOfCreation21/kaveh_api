ebba8ceb46d535afcf917469229d38bd5f74602a
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-02-08 18:17:55 +0330

    ADD CheckIfUserIsBlocked middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'CheckIfUserIsBlocked' => \App\Http\Middleware\CheckIfUserIsBlocked::class,
         'CheckIfShouldChangePassword' => \App\Http\Middleware\CheckIfShouldChangePassword::class,
         'ShouldBePrimary' => \App\Http\Middleware\ShouldBePrimary::class,
         'AllowedUserClass' => \App\Http\Middleware\AllowedUserClass::class,
---
+++ app/Http/Middleware/CheckIfUserIsBlocked.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckIfUserIsBlocked
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = $request->user();
+        if ($user->is_blocked)
+        {
+            return response([
+                'code' => 90,
+                'message' => 'your account has been blocked',
+                'reason_for_blocking' => $user->reason_for_blocking
+            ]);
+        }
+        return $next($request);
+    }
+}
