8eff5ea11926e2b391c05df4e061dd8a78e97966
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-02-09 18:16:53 +0330

    ADD cart_product table migration

---
+++ database/migrations/2022_02_09_180710_create_cart_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_product', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id', 15);
+            $table->string('cart_id', 15);
+            $table->integer('quantity')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_product');
+    }
+}
