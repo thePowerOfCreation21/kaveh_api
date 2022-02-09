98e1d75b45fb49dc93831a3f17ff50680a25c6bd
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-02-09 17:52:28 +0330

    ADD carts table migration

--- database/migrations/2022_02_09_174630_create_carts_table.php
+++ database/migrations/2022_02_09_174630_create_carts_table.php
@@ -15,7 +15,7 @@ class CreateCartsTable extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('user_id')->unique();
         });
     }
 
