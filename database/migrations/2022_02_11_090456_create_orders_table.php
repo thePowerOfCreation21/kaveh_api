71556d5e8acd10bdedd20462ff9783a6de6e9722
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-02-11 09:58:29 +0330

    ADD orders table migration

---
+++ database/migrations/2022_02_11_090456_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id', 15);
+            $table->integer('amount');
+            $table->json('discount');
+            $table->json('products');
+            $table->date('receipt_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
