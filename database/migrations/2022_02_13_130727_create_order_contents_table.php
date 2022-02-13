fab6727caae4b3796ace223443874106084aaf82
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-02-13 13:24:00 +0330

    ADD order_contents table migration

---
+++ database/migrations/2022_02_13_130727_create_order_contents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_contents', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_id', 15);
+            $table->integer('quantity');
+            $table->integer('amount');
+            $table->string('product_id', 15);
+            $table->json('product');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_contents');
+    }
+}
