df794787a43534f0885bfdfc30bf511876e5a0b8
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-04-18 15:15:36 +0430

    ADDED RandomInformativeProducts logic

--- app/Models/RandomInformativeProductUpdateTime.php
+++ app/Models/RandomInformativeProductUpdateTime.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Abstracts\KeyObjectConfig;
+use App\Exceptions\CustomException;
 
 class RandomInformativeProductUpdateTime extends KeyObjectConfig
 {
@@ -15,4 +16,23 @@ class RandomInformativeProductUpdateTime extends KeyObjectConfig
     protected $default_values = [
         'time' => null
     ];
+
+    /**
+     * @return false|int
+     * @throws CustomException
+     */
+    public function get_time ()
+    {
+        $time = $this->get()->time;
+        return !empty($time) ? strtotime($time) : 0;
+    }
+
+    /**
+     * @return bool
+     * @throws CustomException
+     */
+    public function is_time_to_update (): bool
+    {
+        return time() > ($this->get_time() + 86400);
+    }
 }
