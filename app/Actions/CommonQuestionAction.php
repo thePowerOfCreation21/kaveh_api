f3aa5b074f544fcaa08e5ccabe3e6541b915558b
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-05-10 08:31:25 +0430

    REFACTORED CommonQuestionAction

---
+++ app/Actions/CommonQuestionAction.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Actions;
+
+use App\Exceptions\CustomException;
+use App\Models\CommonQuestion;
+use App\Services\Action;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class CommonQuestionAction extends Action
+{
+    protected array $validation_roles = [
+        'store' => [
+            'question' => 'required|string|max:255',
+            'answer' => 'required|string|max:1500',
+        ],
+        'update' => [
+            'question' => 'string|max:255',
+            'answer' => 'string|max:1500'
+        ]
+    ];
+
+    public function __construct()
+    {
+        $this->model = CommonQuestion::class;
+    }
+
+    /**
+     * @param Request $request
+     * @param array|string $validation_role
+     * @param callable|null $storing
+     * @return mixed
+     * @throws CustomException
+     */
+    public function store_by_request(Request $request, array|string $validation_role = 'store', callable $storing = null): mixed
+    {
+        return parent::store_by_request($request, $validation_role, $storing);
+    }
+
+    /**
+     * @param Request $request
+     * @param array|string $validation_role
+     * @param array $query_addition
+     * @param Model|Builder|null $eloquent
+     * @param array $relations
+     * @param array $order_by
+     * @return object
+     * @throws CustomException
+     */
+    public function get_by_request(Request $request, array|string $validation_role = 'get_query', array $query_addition = [], Model|Builder $eloquent = null, array $relations = [], array $order_by = ['id' => 'DESC']): object
+    {
+        return parent::get_by_request($request, $validation_role, $query_addition, $eloquent, $relations, $order_by);
+    }
+
+    /**
+     * @param string $id
+     * @param array $query
+     * @param array $relations
+     * @return mixed
+     * @throws CustomException
+     */
+    public function get_by_id(string $id, array $query = [], array $relations = []): mixed
+    {
+        return parent::get_by_id($id, $query, $relations);
+    }
+
+    /**
+     * @param Request $request
+     * @param string $id
+     * @param array|string $validation_role
+     * @return int|bool
+     * @throws CustomException
+     */
+    public function update_by_request_and_id(Request $request, string $id, array|string $validation_role = 'update'): int|bool
+    {
+        return parent::update_by_request_and_id($request, $id, $validation_role);
+    }
+
+    /**
+     * @param string $id
+     * @param array $query
+     * @param callable|null $deleting
+     * @return bool|int|null
+     */
+    public function delete_by_id(string $id, array $query = [], callable $deleting = null): bool|int|null
+    {
+        return parent::delete_by_id($id, $query, $deleting);
+    }
+}
--- app/Http/Controllers/CommonQuestionController.php
+++ app/Http/Controllers/CommonQuestionController.php
@@ -2,46 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\CommonQuestionAction;
+use App\Exceptions\CustomException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use App\Actions\CommonQuestionActions;
 
 class CommonQuestionController extends Controller
 {
-    public function store (Request $request)
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws CustomException
+     */
+    public function store (Request $request): JsonResponse
     {
-        CommonQuestionActions::store_from_request($request);
+        (new CommonQuestionAction())->store_by_request($request);
 
         return response()->json([
             'message' => 'common question stored successfully'
         ]);
     }
 
-    public function get (Request $request)
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws CustomException
+     */
+    public function get (Request $request): JsonResponse
     {
         return response()->json(
-            CommonQuestionActions::get_by_request($request)
+            (new CommonQuestionAction())->get_by_request($request)
         );
     }
 
-    public function get_by_id (string $id)
+    /**
+     * @param string $id
+     * @return JsonResponse
+     * @throws CustomException
+     */
+    public function get_by_id (string $id): JsonResponse
     {
         return response()->json(
-            CommonQuestionActions::get_by_id($id)
+            (new CommonQuestionAction())->get_by_id($id)
         );
     }
 
-    public function edit_by_id (Request $request, string $id)
+    /**
+     * @param Request $request
+     * @param string $id
+     * @return JsonResponse
+     * @throws CustomException
+     */
+    public function edit_by_id (Request $request, string $id): JsonResponse
     {
-        CommonQuestionActions::edit_by_request_and_id($request, $id);
+        (new CommonQuestionAction())->update_by_request_and_id($request, $id);
 
         return response()->json([
             'message' => 'CommonQuestion updated successfully'
         ]);
     }
 
-    public function delete_by_id (string $id)
+    /**
+     * @param string $id
+     * @return JsonResponse
+     */
+    public function delete_by_id (string $id): JsonResponse
     {
-        CommonQuestionActions::delete_by_id($id);
+        (new CommonQuestionAction())->delete_by_id($id);
 
         return response()->json([
             'message' => 'common question deleted successfully'
