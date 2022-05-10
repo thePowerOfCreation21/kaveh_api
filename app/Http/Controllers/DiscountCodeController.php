53b9200a89149af5f1483edeeb9cad75631ef07f
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-05-10 09:30:25 +0430

    REFACTORED DiscountCodeAction

--- app/Http/Controllers/DiscountCodeController.php
+++ app/Http/Controllers/DiscountCodeController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Actions\DiscountCodeAction;
+use App\Exceptions\CustomException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class DiscountCodeController extends Controller
 {
-    public function store (Request $request)
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws CustomException
+     */
+    public function store (Request $request): JsonResponse
     {
         (new DiscountCodeAction())->store_by_request($request);
 
@@ -16,28 +23,48 @@ class DiscountCodeController extends Controller
         ]);
     }
 
-    public function get (Request $request)
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws CustomException
+     */
+    public function get (Request $request): JsonResponse
     {
         return response()->json(
             (new DiscountCodeAction())->get_by_request($request)
         );
     }
 
-    public function get_by_id (string $id)
+    /**
+     * @param string $id
+     * @return JsonResponse
+     * @throws CustomException
+     */
+    public function get_by_id (string $id): JsonResponse
     {
         return response()->json(
             (new DiscountCodeAction())->get_by_id($id)
         );
     }
 
-    public function get_users (Request $request, string $id)
+    /**
+     * @param Request $request
+     * @param string $id
+     * @return JsonResponse
+     * @throws CustomException
+     */
+    public function get_users (Request $request, string $id): JsonResponse
     {
         return response()->json(
             (new DiscountCodeAction())->get_users_by_request_and_discount_id($request, $id)
         );
     }
 
-    public function delete_by_id (string $id)
+    /**
+     * @param string $id
+     * @return JsonResponse
+     */
+    public function delete_by_id (string $id): JsonResponse
     {
         (new DiscountCodeAction())->delete_by_id($id);
 
@@ -46,7 +73,12 @@ class DiscountCodeController extends Controller
         ]);
     }
 
-    public function check_if_user_can_use_the_discount (Request $request)
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws CustomException
+     */
+    public function check_if_user_can_use_the_discount (Request $request): JsonResponse
     {
         $discountCode = (new DiscountCodeAction())->check_if_user_can_use_discount_by_request($request)
             ->details->discountCode;
