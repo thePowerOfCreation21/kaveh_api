c73badb9156b7127f6b47e824592d405205ac16f
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-05-10 09:46:54 +0430

    REFACTORED ArticleAction

--- app/Actions/ArticleAction.php
+++ app/Actions/ArticleAction.php
@@ -185,11 +185,12 @@ class ArticleAction extends Action
      * @param Request $request
      * @param string $id
      * @param array|string $validation_role
+     * @param callable|null $updating
      * @return int|bool
      * @throws CustomException
      */
-    public function update_by_request_and_id (Request $request, string $id, array|string $validation_role = 'update'): int|bool
+    public function update_by_request_and_id (Request $request, string $id, array|string $validation_role = 'update', callable $updating = null): int|bool
     {
-        return $this->update_by_request_and_query($request, ['id' => $id], $validation_role);
+        return $this->update_by_request_and_query($request, ['id' => $id], $validation_role, $updating);
     }
 }
