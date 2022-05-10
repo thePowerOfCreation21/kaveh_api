1fbde1cea47074e956204b036bae9a895c21a2b9
thePowerOfCreation21 <dadekavweb2@gmail.com>
2022-05-10 09:44:21 +0430

    REFACTORED Action

--- app/Services/Action.php
+++ app/Services/Action.php
@@ -409,8 +409,8 @@ abstract class Action
      * @return int|bool
      * @throws CustomException
      */
-    public function update_by_request_and_id (Request $request, string $id, array|string $validation_role = 'update'): int|bool
+    public function update_by_request_and_id (Request $request, string $id, array|string $validation_role = 'update', callable $updating = null): int|bool
     {
-        return $this->update_by_request_and_query($request, ['id' => $id], $validation_role);
+        return $this->update_by_request_and_query($request, ['id' => $id], $validation_role, $updating);
     }
 }
